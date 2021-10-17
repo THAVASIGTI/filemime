b1b3036174c9023afb80bea97934af215ba3e089
thavasi_t <thavasi.t@secneurx.com>
2021-10-17 15:14:00 +0530

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setuptools.setup(
     name="filemime",
-    version="0.0.2",
+    version="0.0.3",
     author="T.THAVASI",
     license="MIT",
     author_email="ganeshanthavasigti1032000@gmail.com",
